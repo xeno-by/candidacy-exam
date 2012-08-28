54c49ed8a8b4a27962ff6f4e2376727b17fc4e83
Eugene Burmako <xeno.by@gmail.com>
2012-08-28 17:18:02 +0200

    wip

--- macros.rkt
+++ macros.rkt
@@ -1,10 +1,31 @@
 #lang racket
 
 (require mzlib/defmacro)
-(defmacro let (decl body)
-  (cons
-   (cons 'lambda
-         (cons (list (car decl)) body))
-   (cdr decl)))
 
-(let (x 42) (print x))
+;;(defmacro let (decl body)
+;;  (cons
+;;   (cons 'lambda
+;;         (cons (list (car decl)) body))
+;;   (cdr decl)))
+
+;;(defmacro let (decl body)
+;;  `((lambda (,(car decl)) ,body) ,(cadr decl)))
+
+;;(defmacro let (decls body)
+;;  (cons
+;;   (cons 'lambda
+;;         (cons (map car decls)
+;;               body))
+;;   (map cadr decls)))
+
+;;(defmacro let args
+;;  (cons
+;;   (cons 'lambda
+;;         (cons (map car (car args))
+;;               (cdr args)))
+;;   (map cadr (car args))))
+
+(defmacro let (decls body)
+  `((lambda ,(map car decls) ,body) . ,(map cadr decls)))
+
+(let ((x 40) (y 2)) (print (+ x y)))
