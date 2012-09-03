4bd1957dc90dc101f36247e9661ab8f843652f90
Eugene Burmako <xeno.by@gmail.com>
2012-09-03 16:38:59 +0200

    quasiquote

--- macros2.rkt
+++ macros2.rkt
@@ -2,13 +2,18 @@
 
 (require mzlib/defmacro)
 
+;;(defmacro aif args
+;;  (list 'let 
+;;        (list (list 'it (car args)))
+;;        (list 'if
+;;              'it
+;;              (cadr args)
+;;              (caddr args))))
 (defmacro aif args
-  (list 'let 
-        (list (list 'it (car args)))
-        (list 'if
-              'it
-              (cadr args)
-              (caddr args))))
+  `(let (it ,(car args))
+     (if it
+         ,(cadr args)
+         ,(caddr args))))
 
 (define calculate (lambda () 42))
 (aif (calculate)
