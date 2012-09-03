c3ff1961b0ac5353d2c11e012a654a356b582917
Eugene Burmako <xeno.by@gmail.com>
2012-09-03 23:55:27 +0200

    minor

---
+++ macros3.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(define calculate (lambda () 42))
+
+(require mzlib/defmacro)
+;;(defmacro aif args
+;;(list 'let* (list (list 'temp
+;;(car args))
+;;(list 'it 'temp))
+;;(list 'if 'temp
+;;(cadr args)
+;;(caddr args))))
+(defmacro aif args
+`(let*
+((temp ,(car args))
+(it
+temp))
+(if
+temp
+,(cadr args)
+,(caddr args))))
+
+(aif (calculate)
+(print it)
+(error "does not compute"))
