2bf93cb5619a23bf9ccbd5be006b0e48b4ed2eed
Eugene Burmako <xeno.by@gmail.com>
2012-09-04 15:33:45 +0200

    racket fin and trilogy fin

--- macros4.rkt
+++ macros4.rkt
@@ -1,14 +1,26 @@
 #lang racket
 
-(define-syntax (aif stx)
-  (syntax-case stx ()
+;;(define-syntax (aif stx)
+;;  (syntax-case stx ()
+;;    ((aif test then else)
+;;     (with-syntax ((it (datum->syntax #'aif 'it)))
+;;       #'(let* ((temp test)
+;;                (it temp))
+;;           (if temp then else))))))
+
+(require racket/stxparam)
+
+(define-syntax-parameter it (syntax-rules ()))
+
+(define-syntax aif
+  (syntax-rules ()
     ((aif test then else)
-     (with-syntax ((it (datum->syntax #'aif 'it)))
-       #'(let* ((temp test)
-                (it temp))
-           (if temp then else))))))
+     (let* ((temp test))
+       (syntax-parameterize
+        ((it (syntax-rules () ((_) temp))))
+        (if temp then else))))))
 
 (define calculate (lambda () 42))
 
-(aif (calculate) (print it) (error "does not compute"))
+(aif (calculate) (print (it)) (error "does not compute"))
  
