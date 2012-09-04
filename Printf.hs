a8a2e6a737dae913edaf0502f6c3b336bb9f5276
Eugene Burmako <xeno.by@gmail.com>
2012-09-04 12:16:33 +0200

    template haskell

--- Printf.hs
+++ Printf.hs
@@ -4,16 +4,18 @@ import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
 
 aif :: Q Exp -> Q Exp -> Q Exp -> Q Exp
-aif if'' then'' else'' =
+--aif if' then' else' =
 --  [| let temp = $if'
 --         it = temp
 --     in if temp /= 0 then $then' else $else' |]
+aif if'' then'' else'' =
     do { temp <- newName "temp"
-       ; it <- newName "it"
+--       ; it <- newName "it"
        ; if' <- if''
        ; then' <- then''
        ; else' <- else''
        ; let notEq = mkNameG_v "ghc-prim" "GHC.Classes" "/="
+             it = mkName "it"
          in return (LetE [ValD (VarP temp) (NormalB if') [],
                        ValD (VarP it) (NormalB (VarE temp)) []]
                       (CondE (InfixE (Just (VarE temp)) (VarE notEq) (Just (LitE (IntegerL 0)))) then' else'))
